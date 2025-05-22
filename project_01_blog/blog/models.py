be268c904ca506a4ee01c71aa5cf6ba4d0b4ec70
MohamedSalahdj <mohamedsamydj@gmail.com>
2025-05-22 03:47:58 +0300

    Add PublishedManager to filter published posts in Post model

--- project_01_blog/blog/models.py
+++ project_01_blog/blog/models.py
@@ -2,6 +2,14 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = "DF", 'Draft'
@@ -19,6 +27,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ["-publish"]
