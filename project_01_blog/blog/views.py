e4258240ce7998a23b49946ade904023953d491b
Mohamed Salah <mohamedsamydj@gmail.com>
2025-07-18 03:10:07 +0300

    Edit `post_list` view to make pagination for post by return `5` posts per page

--- project_01_blog/blog/views.py
+++ project_01_blog/blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Post
 
 
 def post_list(request):
     posts = Post.published.all()
+
+    page_number = request.GET.get("page", 1)
+    paginator = Paginator(posts, 5)
+    
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
