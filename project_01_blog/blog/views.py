38b5bf12a999a712f735ccd5dfee008e28b7373e
Mohamed Salah <mohamedsamydj@gmail.com>
2025-07-22 18:09:37 +0300

    Add `post_share` view with URL Path to handle share posts via emails

--- project_01_blog/blog/views.py
+++ project_01_blog/blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -37,3 +39,46 @@ def post_details(requst, year, month, day, post):
         publish__day=day
     )
     return render(requst, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post.published, id=post_id
+    )
+
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']})"
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent=True
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent
+        }
+    )
