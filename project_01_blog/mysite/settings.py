32d09cf834929815af45b0cad5729822f599303f
Mohamed Salah <mohamedsamydj@gmail.com>
2025-08-05 17:11:08 +0300

    Add `taggit` to install INSTALLED_APPS and update requirements.txt to sync with new packages installed

--- project_01_blog/mysite/settings.py
+++ project_01_blog/mysite/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    # Third-party packages
+    'taggit',
+
     # Project Apps
     'blog.apps.BlogConfig',
     
