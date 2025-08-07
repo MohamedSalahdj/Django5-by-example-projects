2bee180ef5b5d875e0dac296d45fe191d1e7df69
Mohamed Salah <mohamedsamydj@gmail.com>
2025-08-07 03:30:36 +0300

    Add `get_most_commented_posts` custom template tag

--- project_01_blog/blog/templatetags/blog_tags.py
+++ project_01_blog/blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from ..models import Post
 
@@ -16,3 +17,10 @@ def total_posts():
 def show_latest_posts(count=5):
     lates_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': lates_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
