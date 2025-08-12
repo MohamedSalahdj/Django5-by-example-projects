7b1968447994b2574b65ccbd044b87cc4e83650f
Mohamed Salah <mohamedsamydj@gmail.com>
2025-08-12 04:33:53 +0300

    Implement search functionality for blog posts with a dedicated view and template

--- project_01_blog/blog/views.py
+++ project_01_blog/blog/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from taggit.models import Tag
 from django.db.models import Count
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.views.generic import ListView
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_POST
+from django.contrib.postgres.search import SearchVector
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.views.generic import ListView
-from django.core.mail import send_mail
-from taggit.models import Tag
 
-from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .models import Post
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 
 def post_list(request, tag_slug=None):
@@ -142,5 +143,31 @@ def post_comment(request, post_id):
             'post': post,
             'form': form,
             'comment': comment
+        }
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    search=SearchVector('title', 'body'),
+                ).filter(search=query)
+            )
+    
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
         }
     )
