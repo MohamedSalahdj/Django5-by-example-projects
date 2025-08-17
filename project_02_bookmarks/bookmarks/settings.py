c069810b7114aea0b828a260de768828d5b30211
Mohamed Salah <mohamedsamydj@gmail.com>
2025-08-17 06:02:05 +0300

    Add authentication URL settings for login, logout, and redirect

--- project_02_bookmarks/bookmarks/settings.py
+++ project_02_bookmarks/bookmarks/settings.py
@@ -124,3 +124,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'account:dashboard'
+LOGIN_URL = 'account:login'
+LOGOUT_URL = 'account:logout'
