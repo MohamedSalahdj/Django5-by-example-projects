5afe54b8b5823b742c18c4585eb23f4983170687
Mohamed Salah <mohamedsamydj@gmail.com>
2025-08-17 06:34:48 +0300

    Add password change form and done templates, and update URLs for password change functionality

--- project_02_bookmarks/account/urls.py
+++ project_02_bookmarks/account/urls.py
@@ -3,11 +3,12 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 
-app_name = 'account'
 
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('pathword-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
