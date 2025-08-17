b9ce41ba54dd5b33eb6c160368666d497852fade
Mohamed Salah <m.salah@proactai.net>
2025-08-17 23:39:09 +0300

    Fix password confirmation field name in registration form and enhance validation logic

--- project_02_bookmarks/account/froms.py
+++ project_02_bookmarks/account/froms.py
@@ -1,6 +1,29 @@
 from django import forms
-
+from django.contrib.auth import get_user_model
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(
+        label= 'password',
+        widget= forms.PasswordInput
+    ) 
+    passwod2 = forms.CharField(
+        label= 'Repeat Password',
+        widget= forms.PasswordInput
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'email']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password2 = cleaned_data.get('password2')
+
+        if password and password2 and password != password2:
+            raise forms.ValidationError("Passwords don't match")
