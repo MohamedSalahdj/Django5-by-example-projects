f8040f1fd3d09582b66549927538815bb36f74b5
Mohamed Salah <m.salah@proactai.net>
2025-08-18 03:57:32 +0300

    Add ProfileEditForm for editing user profile details

--- project_02_bookmarks/account/froms.py
+++ project_02_bookmarks/account/froms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
+from .models import Profile
+
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
@@ -27,3 +29,16 @@ class RegistrationForm(forms.ModelForm):
 
         if password and password2 and password != password2:
             raise forms.ValidationError("Passwords don't match")
+        
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email']
+
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
